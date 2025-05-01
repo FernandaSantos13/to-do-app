699fbb3f54f618f852ee64c1803678a5443d1d17
Marvio Bezerra <marvio.bezerra@gmail.com>
2025-05-01 17:28:06 -0400

    Reference

--- src/App.tsx
+++ src/App.tsx
@@ -2,68 +2,60 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import { RenderList } from './RenderList.tsx';
 
-type ToDo = {
-  id: number;
-  text: string;
-  done: boolean;
-};
-
-
+import { ToDo, api } from './apiClient';
 
 function App() {
   const [newtoDo, setNewtoDo] = useState<string>("");
   const [toDoList, setToDoList] = useState<ToDo[]>([]);
-  const [editIndex, setEditIndex] = useState<number | null>(null);
-  const [editToDo, setEditToDo] = useState<string>("");
 
   useEffect(() => {
-    fetch("http://localhost:3000/todos")
-      .then((res) => res.json()) 
-      .then((data: ToDo[]) => {
+    api.getAll()
+      .then((data) => {
         setToDoList(data);
       })
       .catch((err) => {
         console.error("Error fetching ToDos:", err);
       });
-  }, []); 
-   
+  }, []);
 
+  const handleSave = async () => {
+    if (!newtoDo.trim()) {
+      return;
+    }
 
-  const handleDelete = (index: number) => {
-    const updatedList = toDoList.filter((_, i) => i !== index);
-    setToDoList(updatedList);
-  };
-  
-  const handleToggleDone = (index: number) => {
-    const updatedList = toDoList.map((toDo, i) => {
-      if (i === index) {
-        return { ...toDo, done: !toDo.done }; 
-      }
-      return toDo;
-    });
-    setToDoList(updatedList);
-  };
+    const newToDo = { id: -1, text: newtoDo, done: false };
+    const serverToDo = await api.save(newToDo)
+    setToDoList([...toDoList, serverToDo]);
+  }
 
-  const handleEdit = (index: number) => {
-    setEditIndex(index);
-    setEditToDo(toDoList[index].text);
+  const handleDelete = async (todo: ToDo) => {
+    const result = await api.delete(todo);
+
+    if (result) {
+      const updatedList = toDoList.filter((item) => todo.id !== item.id);
+      setToDoList(updatedList);
+    }
   };
 
-  const handleSaveEdit = () => {
-    if (editIndex === null) return;
-
-    const updatedList = toDoList.map((toDo, i) => {
-      if ( i === editIndex) {
-        return { ...toDo, text: editToDo }; 
-      }
-      return toDo;
+  const handleToggleDone = async (todo: ToDo) => {
+    const result = await api.update({
+      ...todo,
+      done: !todo.done
     });
 
+    const updatedList = toDoList.map((item) => item.id === todo.id ? result : item);
+
     setToDoList(updatedList);
-    setEditIndex(null);
-    setEditToDo("");
   };
-  
+
+  const handleSaveEdit = async (todo: ToDo) => {
+    const result = await api.update(todo);
+
+    const updatedList = toDoList.map((item) => item.id === todo.id ? result : item);
+
+    setToDoList(updatedList);
+  };
+
   return (
     <>
       <h1>To Do List App</h1>
@@ -75,41 +67,19 @@ function App() {
           rows={2}
           cols={30}
         />
-        <button onClick={() => {
-          if(!newtoDo.trim()) return;
-          const newToDo = { text: newtoDo, done: false };
-          fetch("http://localhost:3000/todos", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newToDo),
-          })
-          .then((res) => res.json())
-          .then((data: ToDo) => {
-            setToDoList((prevList) => [...prevList, data]);
-            setNewtoDo(""); 
-          })
-          .catch((err) => {
-            console.error("Error adding ToDo:", err);
-          });
-        }}>
+        <button onClick={handleSave}>
           Add ToDo
         </button>
       </div>
       <div className="to-do-list">
         <h2>ToDo List</h2>
-        <ol> 
-          <RenderList 
-            toDoList = {toDoList} 
-            onDelete={handleDelete} 
-            onToggleDone={handleToggleDone}
-            onEdit={handleEdit}
-            onEditToDo={setEditToDo}
+        <ol>
+          <RenderList
+            toDoList={toDoList}
+            onDelete={handleDelete}
+            onToggleDone={handleToggleDone}           
             onSaveEdit={handleSaveEdit}
-            editIndex={editIndex}
-            editToDo={editToDo}          
-            />
+          />
         </ol>
       </div>
     </>
